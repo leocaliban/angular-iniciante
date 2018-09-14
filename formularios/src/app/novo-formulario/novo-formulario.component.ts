13b01db61e9c54a28354a28adaed420f6fce9871
Leocaliban <leocaliban@gmail.com>
2018-09-13 21:14:59 -0300

    Aplica two-way binding com ngModel no formulário

--- formularios/src/app/novo-formulario/novo-formulario.component.ts
+++ formularios/src/app/novo-formulario/novo-formulario.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-novo-formulario',
   templateUrl: './novo-formulario.component.html',
   styleUrls: ['./novo-formulario.component.css']
 })
 export class NovoFormularioComponent implements OnInit {
+
+  cliente = new Cliente();
 
   profissaoPadrao = 'Empresário';
 
@@ -18,7 +26,12 @@ export class NovoFormularioComponent implements OnInit {
   }
 
   salvar(form: NgForm) {
-    console.log(this.profissaoPadrao);
+    // Substituido pelo two-way binding
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+
+    console.log(this.cliente);
   }
 
 }
